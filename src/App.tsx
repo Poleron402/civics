5d01813a3fe526b3690fd2afc0dda23e52250fe0
Polina <zhperon@gmail.com>
2025-01-13 15:42:56 -0800

    troubleshooting gh pages mishaps

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './index.css'
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="https://Poleron402.github.io/civics">
       <Routes>
         <Route path="/" element={<StartingPage/>}/>
         <Route path="quiz/:state/" element={<Quiz/>}/>
