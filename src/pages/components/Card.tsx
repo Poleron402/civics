e28c98da2962d039b2abaedb1f56f690a3de3769
Polina <zhperon@gmail.com>
2025-01-12 23:18:40 -0800

    mvp done

--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -10,33 +10,66 @@ interface ScoreProps{
 const Card: React.FC<ScoreProps> = ({score, setScore, question})=>{
     const [isActive, setIsActive] = useState<boolean>(false);
     let [questionCounter, setQuestionCounter] = useState<number>(0);
-    console.log(question)
+    let [tempHidden, setTempHidden] = useState<boolean>(false);
+
+    const handleAnswer = (correct: boolean)=>{
+        setTempHidden(true);
+        setIsActive(false)
+        setTimeout(() => {
+            setScore(correct ? score + 1 : score); // Update the score if correct
+            setQuestionCounter(questionCounter + 1); // Increment the question counter
+          }, 300);
+    }
+
     return (
-        <div id='card' className={isActive ? 'active' : ''} onClick={()=>setIsActive(!isActive)}>
-            <div id='front'>
-                <div id='content'>
-                    {question[questionCounter].question}
+        <>
+        {questionCounter<=9?(
+        <>
+            <div id='card' className={isActive ? 'active' : ''}>
+                <div id='front' onClick={()=>{
+                    setTempHidden(false)
+                    setIsActive(true)}}>
+                    <div id='content'>
+                        {question[questionCounter].question}
+                    </div>
+                </div>
+                <div id='back' onClick={()=>setIsActive(false)}>
+                    {!tempHidden &&
+                    <div id='content' className="flex justify-center items-center h-full">
+
+                    <ol className="flex-col justify-center items-center">
+                        {question[questionCounter].answers.map((answ, ind)=>
+                            <li className="text-center">{answ}</li>
+                        )}
+                    </ol>
+                    </div>
+                    }
                 </div>
             </div>
-            <div id='back'>
-                <div className='flex justify-end absolute top-0 right-0'>
-                    Did you get it?
+            {isActive&&
+            <div className='flex justify-end pt-10'>
+                    Did you get it? 
                     <Check onClick={()=>{
-                        setScore(score+1)
-                        setQuestionCounter(questionCounter+=1) 
+                        handleAnswer(true)
                         }}/>
                     <X onClick={()=>
-                        setQuestionCounter(questionCounter+=1)}/>
-                </div>
-                <div id='content' className="flex justify-center items-center h-full">
-                <ol className="flex-col justify-center items-center">
-                    {question[questionCounter].answers.map((answ, ind)=>
-                        <li className="text-center">{answ}</li>
-                    )}
-                </ol>
-                </div>
+                        handleAnswer(false)
+                    }/>
             </div>
-        </div>
+            }
+        </>
+        ):(
+            <>
+                {score>=6?
+                    <h1>Congrats</h1>
+                :
+                    <h1>Sucker</h1>
+                
+                }
+            </>
+        )
+    }
+    </>
     )
 }
 
