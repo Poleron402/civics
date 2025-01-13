eb26cebf8f25731ad5a8ed2565d701fdaf7484b6
Polina <zhperon@gmail.com>
2025-01-12 21:07:25 -0800

    something is working

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,16 +1,16 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import data from '../data/questions.json'
 import Card from "./components/Card";
-interface Question {
-    question: string;
-    answers: string[];
-}
+import { Question } from "../types";
 const Quiz = () =>{
     const [testStart, setTestStart] = useState<boolean>(false);
     const [questions, setQuestions] = useState<Question[]>([]);
+    let [questionCounter, setQuestionCounter] = useState<number>(0);
+    let [score, setScore] = useState<number>(0);
 
-    console.log(data)
+
+    // console.log(data)
     let {state} = useParams()
 
     const getQuestionsAndAnswers = () =>{
@@ -19,16 +19,22 @@ const Quiz = () =>{
             setQuestions([...questions, data[element]])
         });
     }
+    useEffect(()=>{
+        getQuestionsAndAnswers()
+    }, [])
     return (
         <>
         <div className="flex-col w-full  justify-items-center">
             <h2 className="text-3xl pt-10">Hi there in {state}</h2>
             <p className="text-sm border-b border-gray-300">*State adjusted answers are in the works*</p>
             {!testStart ?
-            <button className="mt-10 hover:bg-red-800" onClick = {()=>setTestStart(true)}>Begin Learning</button>
+                <button className="mt-10 hover:bg-red-800" onClick = {()=>setTestStart(true)}>Begin Learning</button>
             :
             (
-                <Card/>
+                <>
+                    <Card score={score} setScore={setScore} question={questions[questionCounter]} />
+                    <p className="pt-6">Score: {score} (6 to pass)</p>
+                </>
             )
             }
         </div>
--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -1,26 +1,37 @@
 import { Check, X } from "lucide-react"
 import { useState } from "react";
-const Card = ()=>{
+import { Question } from "../../types";
+
+interface ScoreProps{
+    score: number;
+    setScore: React.Dispatch<React.SetStateAction<number>>;
+    question: Question;
+}
+const Card: React.FC<ScoreProps> = ({score, setScore, question})=>{
     const [isActive, setIsActive] = useState<boolean>(false);
 
     return (
         <div id='card' className={isActive ? 'active' : ''} onClick={()=>setIsActive(!isActive)}>
             <div id='front'>
-                <div>
-                    <Check/>
-                    <X/>
-                </div>
                 <div id='content'>
-                    Side Two
+                    {question.question}
                 </div>
             </div>
             <div id='back'>
-                <div>
-                    <Check/>
+                <div className='flex justify-end absolute top-0 right-0'>
+                    Did you get it?
+                    <Check onClick={()=>{
+
+                        setScore(score+1)
+                        }}/>
                     <X/>
                 </div>
-                <div id='content'>
-                    Side One
+                <div id='content' className="flex justify-center items-center h-full">
+                <ol className="flex-col justify-center items-center">
+                    {question.answers.map((answ, ind)=>
+                        <li className="text-center">{answ}</li>
+                    )}
+                </ol>
                 </div>
             </div>
         </div>
