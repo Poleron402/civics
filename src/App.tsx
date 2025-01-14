0d377ea6d00fe869d43428f5f9f7faea946b9e5c
Polina <zhperon@gmail.com>
2025-01-14 15:20:09 -0800

    fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import StartingPage from './pages/StartingPage'
 import Quiz from "./pages/Quiz";
 import './index.css'
 function App() {
+
   return (
-    // <HashRouter >
       <Routes>
-        <Route path="/civics/" element={<StartingPage/>}/>
-        <Route path="/civics/quiz/:state/" element={<Quiz/>}/>
+        <Route path="/" element={<StartingPage/>}/>
+        <Route path="/quiz/:state/" element={<Quiz/>}/>
       </Routes>
-    // </HashRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <HashRouter basename="/civics">
+    <BrowserRouter basename="/civics/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
 );
