291d4c77212a8d90e341aca93b0d3ac44380aaeb
Polina <zhperon@gmail.com>
2025-01-14 11:26:52 -0800

    changing somethjing as a test

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <BrowserRouter basename="/civics">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
