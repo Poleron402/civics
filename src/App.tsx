5cae47a093007d5f6d04dd3c19a38f9c4ed1b610
Polina <zhperon@gmail.com>
2025-01-14 18:08:48 -0800

    troubleshooting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import StartingPage from './pages/StartingPage'
 import Quiz from "./pages/Quiz";
 import './index.css'
 function App() {
-
+  useEffect(() => {
+    document.title = 'Civics Quiz App'; // Set the title dynamically
+  }, []); 
   return (
       <Routes>
         <Route path="/" element={<StartingPage/>}/>
