3c7b705a7d8c3f7c6f79908bad21f24b9eb4005c
Polina <zhperon@gmail.com>
2025-01-31 17:46:05 -0800

    removing a non-working file

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import App from './App'
-import '@testing-library/jest-dom';
-
-
-test('renders correct elements', ()=>{
-    render(<App/>);
-    expect(screen.getByText('Pick your State')).toBeInTheDocument();
-})
